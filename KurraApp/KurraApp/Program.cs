f0ea86d2e27d6e36eed943995a9775d386eb9e67
Volkan Şener <159662095+volkansenerrr@users.noreply.github.com>
2024-06-25 18:47:50 +0300

    BasitBirKurraUygulamasiEklendi

---
+++ KurraApp/KurraApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurraApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan kaç kişinin katılacağını al
+            Console.WriteLine("Kuraya kaç kişi katılacak?");
+            int kisiSayisi = Convert.ToInt32(Console.ReadLine());
+
+            // Konsolu temizle
+            Console.Clear();
+
+            // Kişi isimlerini tutmak için bir dizi oluştur
+            string[] kisiler = new string[kisiSayisi];
+
+            // Kullanıcıdan kişi isimlerini al
+            for (int i = 0; i < kisiSayisi; i++)
+            {
+                Console.WriteLine("Kişi adı giriniz:");
+                kisiler[i] = Console.ReadLine();
+            }
+
+            // Konsolu tekrar temizle
+            Console.Clear();
+
+            // Kayıt tamamlandı mesajı göster
+            Console.WriteLine("Kayıt tamamlandı");
+
+            // Rastgele sayı üretmek için Random sınıfından bir nesne oluştur
+            Random rnd = new Random();
+
+            // 0 ile dizi uzunluğu arasında rastgele bir index seç
+            int index = rnd.Next(0, kisiler.Length);
+
+            // Seçilen kişiyi ekrana yazdır
+            Console.WriteLine("Seçilmiş olan = " + kisiler[index]);
+        }
+    }
+}
